e7224c1f8d18453b46699d836e797005a767b3ae
Kent Hiroi <khiroi@mail.sfsu.edu>
2023-01-17 23:09:19 -0800

    Updated to show names on chara/kart pages, along with costume names

--- src/routes/characters/[id]/+page.svelte
+++ src/routes/characters/[id]/+page.svelte
@@ -1,19 +1,20 @@
 <script>
     import ItemMenu from "$lib/components/ItemMenu.svelte";
     import Item from "$lib/components/Item.svelte";
+    import enUSLocale from "$lib/locale/en-US.json"
 
     export let data;
 </script>
 
 <div class="full-bleed grid">
-    <h1>{data.name}</h1>
+    <h1>{enUSLocale.String[`Item_Character:${data.id}_NAME`]}</h1>
     <img src={data.imgSrc} alt="" width="408" height="380" loading="lazy" />
 
     {#if data.costumes.length > 0}
         <h2>Costumes</h2>
         <ItemMenu itemWidth="120">
             {#each data.costumes as costume}
-                <Item imgSrc={costume.imgSrc} name={costume.name} />
+                <Item imgSrc={costume.imgSrc} name={enUSLocale.String[`Item_Costume:${costume.id}_NAME`]} />
             {/each}
         </ItemMenu>
     {/if}
--- src/routes/karts/[id]/+page.svelte
+++ src/routes/karts/[id]/+page.svelte
@@ -1,9 +1,11 @@
 <script>
+    import enUSLocale from "$lib/locale/en-US.json"
+
     export let data;
 </script>
 
 <div class="full-bleed grid">
-    <h1>{data.name}</h1>
+    <h1>{enUSLocale.String[`Item_Kart:${data.id}_NAME`]}</h1>
     <img src={data.imgSrc} alt="" width="408" height="380" loading="lazy" />
 </div>
 
