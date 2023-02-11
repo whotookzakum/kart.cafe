d789e55c875ccbead2d1a1bbaf5975115bd2e90b
Zakum <mirveo@gmail.com>
2023-02-11 09:06:06 -0800

    move item mode drop tables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
     import MetaTags from "$lib/components/MetaTags.svelte";
     import Changelog from "$lib/components/misc/Changelog.svelte";
-    import ItemDropTable from "$lib/components/misc/ItemDropTable.svelte";
     import YouTube from "../lib/components/YouTube.svelte";
 </script>
 
@@ -26,15 +25,3 @@
 
 <h2>What's New</h2>
 <Changelog />
-
-<h2>Item Mode drop rates (8 players)</h2>
-
-<h3>Squad</h3>
-<ItemDropTable mode="squad" />
-
-<h3>Duo</h3>
-<ItemDropTable mode="duo" />
-
-<h3>Solo</h3>
-<ItemDropTable mode="solo" />
-
---
+++ src/routes/tracks/[id]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import ItemDropTable from "$lib/components/misc/ItemDropTable.svelte";
+</script>
+
+<h2>Item Mode drop rates (8 players)</h2>
+
+<h3>Squad</h3>
+<ItemDropTable mode="squad" />
+
+<h3>Duo</h3>
+<ItemDropTable mode="duo" />
+
+<h3>Solo</h3>
+<ItemDropTable mode="solo" />
