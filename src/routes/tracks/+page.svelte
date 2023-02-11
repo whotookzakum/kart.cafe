28b48fe41cd9e67965d46759e0d36e6004d6b999
Zakum <mirveo@gmail.com>
2023-02-11 10:15:37 -0800

    added track filters

--- src/routes/tracks/+page.svelte
+++ src/routes/tracks/+page.svelte
@@ -4,43 +4,55 @@
     import _ from "lodash";
 
     export let data;
-    const tracks = data.items;
 
-    const themes = _.uniqBy(data.items, (track) => track.theme).map(
-        (track) => track.theme
-    );
-    const difficulties = [1, 2, 3, 4, 5];
-    const modes = ["Speed", "Item"];
-    const licenses = ["B2", "B1", "L3"]
+    const activeFilters = {
+        theme: "All",
+        difficulties: [1, 2, 3, 4, 5],
+        showItemTracks: true,
+        licenses: ["None", "B2", "B1", "L3"]
+    }
+
+    $: tracks = 
+        data.items
+        .filter(track => activeFilters.theme === "All" || activeFilters.theme.includes(track.theme))
+        .filter(track => activeFilters.difficulties.includes(track.difficulty) || activeFilters.difficulties.length === 5)
+        .filter(track => activeFilters.showItemTracks && track.itemMode || !track.itemMode)
+        .filter(track => activeFilters.licenses.includes(track.reqLicense || "None"))
+
+
+    function resetFilters() {
+        activeFilters.theme = "All"
+        activeFilters.difficulties = [1, 2, 3, 4, 5]
+        activeFilters.showItemTracks = true
+        activeFilters.licenses = ["None", "B2", "B1", "L3"]
+    }
+
 </script>
 
 <div>
-    <select name="" id="">
+    <button class="box surface" on:click={resetFilters}>Reset</button>
+    <select name="" id="" bind:value={activeFilters.theme}>
         <option value="All">All</option>
-        {#each themes as theme}
+        {#each _.uniqBy(data.items, (t) => t.theme).map((t) => t.theme) as theme}
             <option value={theme}>{theme}</option>
         {/each}
     </select>
-    <br/>
+    <br />
     Difficulty:
-    {#each difficulties as difficulty}
+    {#each [1, 2, 3, 4, 5] as difficulty}
         <label>
-            <input type="checkbox" name="difficulty" value={difficulty} />
+            <input type="checkbox" name="difficulty" value={difficulty} bind:group={activeFilters.difficulties} />
             {difficulty}
         </label>
     {/each}
-    <br/>
-    Mode:
-    {#each modes as mode}
+    <br />
+    Show Item-mode Tracks:
+    <input type="checkbox" name="showItemTracks" bind:checked={activeFilters.showItemTracks} />
+    <br />
+    Required License: 
+    {#each ["None", "B2", "B1", "L3"] as license}
         <label>
-            <input type="checkbox" name="gameModes" value={mode} />
-            {mode}
-        </label>
-    {/each}
-    <br/>
-    {#each licenses as license}
-        <label>
-            <input type="checkbox" name="minLicense" value={license} />
+            <input type="checkbox" name="minLicense" value={license} bind:group={activeFilters.licenses} />
             {license}
         </label>
     {/each}
